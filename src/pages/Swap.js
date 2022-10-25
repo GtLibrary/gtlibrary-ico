e682ae90ff158914cfdaac525e7545027481d7ad
johnrraymond <johnrraymond@yahoo.com>
2022-10-25 14:35:21 -0700

    swapped the order to be bog to avax on the page so once someone has bog they can easily convert to avax.
    swapped the in/out order of the tokens in the widget.

--- src/pages/Swap.js
+++ src/pages/Swap.js
@@ -5,7 +5,7 @@ function Swap() {
     return (
         <div id="swap">
             <div className="container-flex marginAuto">
-            <iframe title="swapplatform" src="https://app.bogged.finance/avax/swap?tokenIn=AVAX&tokenOut=0xB09FE1613fE03E7361319d2a43eDc17422f36B09&embed=1&theme=dark" height="780px" width="100%" style={{marginTop: "40px"}}></iframe>
+            <iframe title="swapplatform" src="https://app.bogged.finance/avax/swap?tokenIn=0xB09FE1613fE03E7361319d2a43eDc17422f36B09&tokenOut=AVAX&embed=1&theme=dark" height="780px" width="100%" style={{marginTop: "40px"}}></iframe>
             </div>
         </div>
     )
