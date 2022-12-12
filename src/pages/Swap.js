98aedc9c3552d7e3966f2528dbbf73ca5adf4f23
alzmob <yunayamda3@gmail.com>
2022-12-12 22:13:29 +0900

    update loading screen on swap page

--- src/pages/Swap.js
+++ src/pages/Swap.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import LoadingOverlay from "react-loading-overlay";
+LoadingOverlay.propTypes = undefined;
 import "../styles/Swap.css";
 
 function Swap() {
-    return (
-        <div id="swap">
-            <div className="container-flex marginAuto">
-            <iframe title="swapplatform" src="https://app.bogged.finance/avax/swap?tokenIn=0xB09FE1613fE03E7361319d2a43eDc17422f36B09&tokenOut=AVAX&embed=1&theme=dark" height="780px" width="100%" style={{marginTop: "40px"}}></iframe>
-            </div>
+  const [loading, setLoading] = useState(true);
+  return (
+    <div id="swap">
+      {loading && (
+        <div
+          style={{
+            background: "#00000055",
+            width: "100%",
+            height: "102%",
+            zIndex: "1000",
+            position: "absolute",
+            top: 0,
+          }}
+        >
+          <LoadingOverlay
+            active={true}
+            spinner
+            text="Loading ..."
+            styles={{
+              overlay: (base) => ({
+                ...base,
+                background: "rgba(255, 255, 255)",
+                position: "absolute",
+                marginTop: "30%",
+              }),
+            }}
+            fadeSpeed={9000}
+          ></LoadingOverlay>
         </div>
-    )
+      )}
+      <div className="container-flex marginAuto">
+        <iframe
+          title="swapplatform"
+          src="https://app.bogged.finance/avax/swap?tokenIn=0xB09FE1613fE03E7361319d2a43eDc17422f36B09&tokenOut=AVAX&embed=1&theme=dark"
+          height="780px"
+          width="100%"
+          onLoad={setLoading(false)}
+          style={{ marginTop: "40px" }}
+        ></iframe>
+      </div>
+    </div>
+  );
 }
 
 export default Swap;
